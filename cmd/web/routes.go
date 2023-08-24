2dbe9b0469873a1bc53bc256298cd2a22a3e2093
Kazuma Sakaguchi <kazumasakaguchi@KazumanoMacBook-Pro.local>
2023-08-24 15:27:25 +0900

    Initial commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/sakaguchi-0725/bookings/pkg/config"
+	"github.com/sakaguchi-0725/bookings/pkg/handlers"
+
+	"github.com/go-chi/chi/v4"
+	"github.com/go-chi/chi/v4/middleware"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/sakaguchi-0725/bookings
+
+go 1.20
+
+require (
+	github.com/alexedwards/scs/v2 v2.5.1
+	github.com/go-chi/chi/v4 v4.0.0-rc1
+	github.com/justinas/nosurf v1.1.1
+)
